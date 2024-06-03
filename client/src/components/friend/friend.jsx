468b637e85755d2fe945decd05c69d7a2c1e98df
roman <zroman938@gmail.com>
2024-06-03 19:33:19 +0300

    first commit

--- client/src/components/friend/friend.jsx
+++ client/src/components/friend/friend.jsx
@@ -9,7 +9,7 @@ function Friend({ user }) {
 
     const [isPhotofn, { isSuccess }] = useGetIsPhotoMutation()
     const [isPhoto, setIsPhoto] = useState()
-    console.log(isPhoto)
+    console.log({isPhoto, 1})
     const getIsPhoto = async () => {
        const isPhoto = await isPhotofn({ id: user.id})
        setIsPhoto(isPhoto)
