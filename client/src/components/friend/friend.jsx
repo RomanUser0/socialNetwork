2b0fc5ae07000e778ba46c1c5073b547d220144b
roman <zroman938@gmail.com>
2024-06-03 20:13:16 +0300

    first commit

--- client/src/components/friend/friend.jsx
+++ client/src/components/friend/friend.jsx
@@ -1,6 +1,4 @@
 import Styles from './friend.module.css'
-import { useGetIsPhotoMutation } from '../../store/photoQueryApi/photoQueryApi'
-import { useEffect, useState } from 'react'
 import { MessageCircle } from 'lucide-react'
 import AvatarDefault from '../../assets/images/defaultAvatar/defaultAvatar.jpg'
 import { Link } from 'react-router-dom'
@@ -10,22 +8,12 @@ import { Link } from 'react-router-dom'
 function Friend({ user }) {
 
 
-    const [isPhotofn, { isSuccess }] = useGetIsPhotoMutation()
-    const [isPhoto, setIsPhoto] = useState()
-    console.log({isPhoto, id: 1})
-    const getIsPhoto = async () => {
-       const isPhoto = await isPhotofn({ id: user.id})
-       setIsPhoto(isPhoto)
-    }
-
-    useEffect(() => {
-        getIsPhoto()
-    },[])
+ 
 
     return (
         <div className={Styles.friend} key={user.id}>
             <div className={Styles.description}>
-                <img width={'40px'} src={isPhoto ? `${import.meta.env.VITE_URL}/api/getPhoto/${user?.id}` : AvatarDefault}></img>
+                <img width={'40px'} src={user.isPhoto ? `${import.meta.env.VITE_URL}/api/getPhoto/${user?.id}` : AvatarDefault}></img>
                 <p>{user.firstname} {user.lastname}</p>
             </div>
             <div>
